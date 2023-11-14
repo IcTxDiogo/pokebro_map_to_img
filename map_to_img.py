dbdf9e30c4ee14cc076c5fd6da86fd921d9e89ac
Santos <ictxdiogo@gmail.com>
2023-11-13 22:21:05 -0400

    feat: fix some items

--- map_to_img.py
+++ map_to_img.py
@@ -1,8 +1,6 @@
-
 import os
 import numpy as np
 from PIL import Image
-
 
 TAM = 256
 TAMBLOCK = 8
@@ -12,82 +10,79 @@ RGB = 3
 BLOCKSIZE = 65536
 
 
-def checkColor(hex):
-    if (hex == "0c"):
-        return [0,102,0] 
-    if (hex == "18"):
-        return [0,204,0]
-    if (hex == "1b"):
-        return [0,204,153]
-    if (hex == "1e"):
-        return [0,255,0]
-    if (hex == "28"):
-        return [51,0,204]
-    if (hex == "29"):
-        return [51,0,255]
-    if (hex == "33"):
-        return [51,102,153]
-    if (hex == "4d"):
-        return [51,0,255]
-    if (hex == "56"):
-        return [102,102,102]
-    if (hex == "72"):
-        return [153,51,0]
-    if (hex == "79"):
-        return [153,102,51]
-    if (hex == "81"):
-        return [153,153,153]
-    if (hex == "ac"):
-        return [204,204,204]
-    if (hex == "b3"):
-        return [204,255,255]
-    if (hex == "ba"):
-        return [255,51,0]
-    if (hex == "c0"):
-        return [255,102,0]
-    if (hex == "cf"):
-        return [255,204,153]
-    if (hex == "d2"):
-        return [255,255,0]
-    if (hex == "d7"):
-        return [255,255,255]
-    return [0,0,0]
+def check_color(hex_number):
+    if hex_number == "0c":
+        return [0, 102, 0]
+    if hex_number == "18":
+        return [0, 204, 0]
+    if hex_number == "1b":
+        return [0, 204, 153]
+    if hex_number == "1e":
+        return [0, 255, 0]
+    if hex_number == "28":
+        return [51, 0, 204]
+    if hex_number == "29":
+        return [51, 0, 255]
+    if hex_number == "33":
+        return [51, 102, 153]
+    if hex_number == "4d":
+        return [51, 0, 255]
+    if hex_number == "56":
+        return [102, 102, 102]
+    if hex_number == "72":
+        return [153, 51, 0]
+    if hex_number == "79":
+        return [153, 102, 51]
+    if hex_number == "81":
+        return [153, 153, 153]
+    if hex_number == "ac":
+        return [204, 204, 204]
+    if hex_number == "b3":
+        return [204, 255, 255]
+    if hex_number == "ba":
+        return [255, 51, 0]
+    if hex_number == "c0":
+        return [255, 102, 0]
+    if hex_number == "cf":
+        return [255, 204, 153]
+    if hex_number == "d2":
+        return [255, 255, 0]
+    if hex_number == "d7":
+        return [255, 255, 255]
+    return [0, 0, 0]
 
 
 array_color_unique = []
 
-for z in range(TAMZMIN,TAMZMAX):
-    array = np.zeros([TAM*TAMBLOCK, TAM*TAMBLOCK, RGB], dtype=np.uint8)
+for z in range(TAMZMIN, TAMZMAX):
+    array = np.zeros([TAM * TAMBLOCK, TAM * TAMBLOCK, RGB], dtype=np.uint8)
     for x in range(TAMBLOCK):
         for y in range(TAMBLOCK):
-            if(z < 10):
+            if z < 10:
                 filename = "./teste/00" + str(x) + "00" + str(y) + "0" + str(z) + ".map"
             else:
                 filename = "./teste/00" + str(x) + "00" + str(y) + str(z) + ".map"
-            if(os.path.exists(filename)):
+            if os.path.exists(filename):
                 array_color = []
-                with open(filename,"rb") as f:
+                with open(filename, "rb") as f:
                     control = 0
-                    while (byte := f.read(1)):
-       
+                    while byte := f.read(1):
+
                         if control >= BLOCKSIZE:
                             break
-                        hexbyte = byte.hex()
-                        if hexbyte not in array_color_unique:
-                            array_color_unique.append(hexbyte)
+                        hex_byte = byte.hex()
+                        if hex_byte not in array_color_unique:
+                            array_color_unique.append(hex_byte)
 
-                        array_color.append(checkColor(hexbyte))
-                        control = control +1
+                        array_color.append(check_color(hex_byte))
+                        control = control + 1
                 f.close()
 
                 for i in range(TAM):
                     for j in range(TAM):
-                        array[y*TAM+j,x*TAM+i] = array_color[j+i*TAM]
+                        array[y * TAM + j, x * TAM + i] = array_color[j + i * TAM]
     img = Image.fromarray(array)
-    if(z < 10):
-        saveFileName = "D:/xxxxxx0" + str(z) + ".png"
-    else: 
-        saveFileName = "D:/xxxxxx" + str(z) + ".png"
+    saveFileName = "D:/" + str(z) + ".png"
     img.save(saveFileName)
-array_color_unique.sort() 
+array_color_unique.sort()
 print(array_color_unique)
